bdaed49afd736135b62b68b250416618a097711a
zeylmz <zeylmz245@gmail.com>
2021-01-31 00:41:14 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden class bir veritabanu tablosudur.
+    public interface IEntity
+    {
+    }
+}
