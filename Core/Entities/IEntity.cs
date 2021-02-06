7737f20fe330a434910ac577f58a859162e1913b
zeylmz <zeylmz245@gmail.com>
2021-02-06 22:02:55 +0300

    6 Şubat 1. Mola

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //IEntity implement eden class bir veritabanu tablosudur.
     public interface IEntity
