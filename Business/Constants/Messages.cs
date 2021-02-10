4a76f2b38780c0e971fa6330120dda66043f1d87
zeylmz <zeylmz245@gmail.com>
2021-02-11 00:04:11 +0300

    10. Gün Ders Sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Ekleme İşleme Başarılı";
+        public static string ProdcutNameInvalid = "Ürün İsmi Geçersiz";        
+        public static string MaintenanceTime = "Sistem Bakımda";
+        public static string ProductsListed = "Ürünler Listelendi";
+    }
+}
